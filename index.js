de06572b9151b3a6db3ff3936be5c689cecae3df
admin <admin@cryptostat.org>
2021-04-29 11:02:10 +1000

    update

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 require('dotenv').config();
-const API_URL = process.env.API_URL;
-const PUBLIC_KEY = process.env.WEB3_PUBLIC_KEY;
-const PRIVATE_KEY = process.env.WEB3_PRIVATE_KEY;
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(API_URL);
-
-const contract = require("./artifacts/contracts/MyNFT.sol/MyNFT.json");
+const { BLOCKCHAIN_RPC_URL, WEB3_PUBLIC_KEY, WEB3_PRIVATE_KEY } = process.env;
 const contractAddress = process.env.CONTRACT_ADDRESS;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(BLOCKCHAIN_RPC_URL);
+const contract = require("./artifacts/contracts/MyNFT.sol/MyNFT.json");
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
 
 async function mintNFT(tokenURI) {
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  const nonce = await web3.eth.getTransactionCount(WEB3_PUBLIC_KEY, 'latest'); //get latest nonce
 
   //the transaction
   const tx = {
-    'from': PUBLIC_KEY,
+    'from': WEB3_PUBLIC_KEY,
     'to': contractAddress,
     'nonce': nonce,
     'gas': 500000,
-    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    'data': nftContract.methods.mintNFT(WEB3_PUBLIC_KEY, tokenURI).encodeABI()
   };
 
   console.log(tx)
 
-  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const signPromise = web3.eth.accounts.signTransaction(tx, WEB3_PRIVATE_KEY);
   signPromise.then((signedTx) => {
     web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
       if (!err) {
-        console.log("The hash of your transaction is: ", hash, "\nCheck Alchemy's Mempool to view the status of your transaction!");
+        const payload = {
+          message: `The hash of your transaction is: ${hash}`,
+          explorer: `${process.env.BLOCKCHAIN_EXPLORER_URL}/tx/${hash}`,
+          transactionHash: hash,
+          tokenURI: tokenURI,
+          address: contractAddress
+        }
+        console.log(payload);
       } else {
         console.log("Something went wrong when submitting your transaction:", err)
       }
