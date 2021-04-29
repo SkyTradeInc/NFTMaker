0c00427675dd047e2ae084cd5b4f911bc6b5fb11
admin <admin@cryptostat.org>
2021-04-29 11:01:31 +1000

    add mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, WEB3_PRIVATE_KEY } = process.env;
+const { BLOCKCHAIN_RPC_URL, WEB3_PRIVATE_KEY } = process.env;
 module.exports = {
    solidity: "0.8.0",
-   defaultNetwork: "ropsten",
+   defaultNetwork: "mumbai",
    networks: {
       hardhat: {},
       ropsten: {
-         url: API_URL,
+         url: BLOCKCHAIN_RPC_URL,
          accounts: [`0x${WEB3_PRIVATE_KEY}`]
-      }
+      },
+      mumbai: {
+         url: BLOCKCHAIN_RPC_URL,
+         accounts: [`0x${WEB3_PRIVATE_KEY}`]
+      },
    },
 }
